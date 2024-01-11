f98ee5a6a6aa0771914d6999519fb806ffb128dc
faom002 <faisal_jobb@outlook.com>
2024-01-11 17:29:37 +0100

    lesson3

---
+++ lesson_03/target/debug/deps/lesson_03-2571d5f82da48cac.d
@@ -0,0 +1,5 @@
+/Users/faisal/Documents/rust/lesson_03/target/debug/deps/liblesson_03-2571d5f82da48cac.rmeta: src/main.rs
+
+/Users/faisal/Documents/rust/lesson_03/target/debug/deps/lesson_03-2571d5f82da48cac.d: src/main.rs
+
+src/main.rs:
---
+++ lesson_03/target/debug/deps/lesson_03-448ce185926432ec.d
@@ -0,0 +1,5 @@
+/Users/faisal/Documents/rust/lesson_03/target/debug/deps/lesson_03-448ce185926432ec: src/main.rs
+
+/Users/faisal/Documents/rust/lesson_03/target/debug/deps/lesson_03-448ce185926432ec.d: src/main.rs
+
+src/main.rs:
